73360539506199bba228455dd532d061b8459e7e
rafaelfccg <rafaelfccg@gmail.com>
2015-10-13 14:31:25 -0300

    starting test at stage 1

--- TheBuggger/GameViewController.swift
+++ TheBuggger/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameSceneTestEnemy") {
+        if let scene = GameScene(fileNamed:"Level1Scene") {
             // Configure the view.
             let skView = self.view as! SKView
             skView.showsFPS = true
