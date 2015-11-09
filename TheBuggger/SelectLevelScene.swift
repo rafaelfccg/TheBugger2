bf1b3bbf132fa47e307e09a6ccdd402cc4f5523e
rafaelfccg <rafaelfccg@gmail.com>
2015-11-09 14:33:15 -0300

    start select level

---
+++ TheBuggger/SelectLevelScene.swift
@@ -0,0 +1,25 @@
+//
+//  SelectLevelScene.swift
+//  TheBuggger
+//
+//  Created by Rafael Gouveia on 11/9/15.
+//  Copyright © 2015 rfccg. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class SelectLevelScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+    
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        <#code#>
+    }
+    override func update(currentTime: CFTimeInterval) {
+    
+    }
+
+}
