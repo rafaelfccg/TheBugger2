963ee21c4babecc84b4e37af0ac09a1cc037abbf
va94 <v.augusto.1994@gmail.com>
2016-01-13 17:41:27 -0300

    Continue boss update

---
+++ TheBuggger/FirstBossGameScene.swift
@@ -0,0 +1,9 @@
+//
+//  FirstBossGameScene.swift
+//  TheBuggger
+//
+//  Created by Victor Augusto Pereira Porciúncula on 1/13/16.
+//  Copyright © 2016 rfccg. All rights reserved.
+//
+
+import Foundation
