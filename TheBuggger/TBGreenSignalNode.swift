248fd40a7e5872ff3f421a1129f0db9a554aee76
va94 <v.augusto.1994@gmail.com>
2016-02-15 17:26:59 -0300

    adding greenSignal

---
+++ TheBuggger/TBGreenSignalNode.swift
@@ -0,0 +1,9 @@
+//
+//  TBGreenSignalNode.swift
+//  TheBuggger
+//
+//  Created by Victor Augusto Pereira Porciúncula on 2/15/16.
+//  Copyright © 2016 rfccg. All rights reserved.
+//
+
+import Foundation
