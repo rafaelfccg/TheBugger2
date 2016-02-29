b0efcccacd0c7cabb0a56fa6754b55d1c136cde7
va94 <v.augusto.1994@gmail.com>
2016-02-29 15:25:58 -0300

    green and yellow signal

--- TheBuggger/TBSlowSignalNode.swift
+++ TheBuggger/TBSlowSignalNode.swift
@@ -2,7 +2,7 @@
 //  TBSlowSignalNode.swift
 //  TheBuggger
 //
-//  Created by Victor Augusto Pereira Porciúncula on 2/16/16.
+//  Created by Victor Augusto Pereira Porciúncula on 2/29/16.
 //  Copyright © 2016 rfccg. All rights reserved.
 //
 
